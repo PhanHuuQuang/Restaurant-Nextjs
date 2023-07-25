5e0dc3f6a3c88974fb8719ad3885fcd0ea54b5bb
PhanHuuQuang <94416397+PhanHuuQuang@users.noreply.github.com>
2023-07-25 10:01:17 +0700

    Notification, Menu

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import Menu from "./Menu";
+import Link from "next/link";
+
+const NavBar = () => {
+  return (
+    <div className="h-12 text-red-500 p-4 flex items-center justify-between border-b-2 border-b-red-500 uppercase">
+      {/* LOGO */}
+      <div className="text-xl">
+        <Link href="/">QPhan</Link>
+      </div>
+      {/* MOBILE MENU */}
+      <div>
+        <Menu />
+      </div>
+    </div>
+  );
+};
+
+export default NavBar;
