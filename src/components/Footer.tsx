9fa75b6125d6d8144e00a158bc53cd33c24c8ac2
PhanHuuQuang <94416397+PhanHuuQuang@users.noreply.github.com>
2023-07-26 10:07:17 +0700

    Navbar and Footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,15 @@
+import Link from "next/link";
 import React from "react";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className="h-1/2 md:h-24 p-4 lg:p-20 xl:p-40 text-red-500 flex items-center justify-between">
+      <Link href="/" className="font-bold text-xl">
+        RESTAURANT
+      </Link>
+      <p>© ALL RIGHTS RESERVED.</p>
+    </div>
+  );
 };
 
 export default Footer;
