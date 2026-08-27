7992311f7aee1dc9ba9413d19d83530bea5d3a50
PhanHuuQuang <phanhuuquang9999@gmail.com>
2026-08-27 17:03:36 +0700

    feat(auth): implement role-based access control for categories and products

--- backend/src/categories/categories.controller.ts
+++ backend/src/categories/categories.controller.ts
@@ -6,16 +6,23 @@ import {
   Param,
   Patch,
   Post,
+  UseGuards,
 } from '@nestjs/common';
 import { CategoriesService } from './categories.service';
 import { CreateCategoryDto } from './dto/create-category.dto';
 import { UpdateCategoryDto } from './dto/update-category.dto';
+import { Roles } from '../shared/decorators/roles.decorator';
+import { Role } from '../../prisma/generated/prisma/enums';
+import { RolesGuard } from '../shared/guards/role.guard';
+import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
 
 @Controller('categories')
+@UseGuards(JwtAuthGuard, RolesGuard)
 export class CategoriesController {
   constructor(private readonly categoriesService: CategoriesService) {}
 
   @Post()
+  @Roles(Role.ADMIN)
   create(@Body() createCategoryDto: CreateCategoryDto) {
     return this.categoriesService.create(createCategoryDto);
   }
@@ -31,6 +38,7 @@ export class CategoriesController {
   }
 
   @Patch(':slug')
+  @Roles(Role.ADMIN)
   update(
     @Param('slug') slug: string,
     @Body() updateCategoryDto: UpdateCategoryDto,
@@ -39,6 +47,7 @@ export class CategoriesController {
   }
 
   @Delete(':slug')
+  @Roles(Role.ADMIN)
   remove(@Param('slug') slug: string) {
     return this.categoriesService.remove(slug);
   }
--- backend/src/products/products.controller.ts
+++ backend/src/products/products.controller.ts
@@ -8,16 +8,23 @@ import {
   Patch,
   Post,
   Query,
+  UseGuards,
 } from '@nestjs/common';
 import { ProductsService } from './products.service';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
+import { Roles } from '../shared/decorators/roles.decorator';
+import { Role } from '../../prisma/generated/prisma/enums';
+import { RolesGuard } from '../shared/guards/role.guard';
+import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
 
 @Controller('products')
+@UseGuards(JwtAuthGuard, RolesGuard)
 export class ProductsController {
   constructor(private readonly productsService: ProductsService) {}
 
   @Post()
+  @Roles(Role.ADMIN)
   create(@Body() createProductDto: CreateProductDto) {
     return this.productsService.create(createProductDto);
   }
@@ -40,6 +47,7 @@ export class ProductsController {
   }
 
   @Patch(':id')
+  @Roles(Role.ADMIN)
   update(
     @Param('id', ParseIntPipe) id: number,
     @Body() updateProductDto: UpdateProductDto,
@@ -48,6 +56,7 @@ export class ProductsController {
   }
 
   @Delete(':id')
+  @Roles(Role.ADMIN)
   remove(@Param('id', ParseIntPipe) id: number) {
     return this.productsService.remove(id);
   }
